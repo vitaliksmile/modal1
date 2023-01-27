6dfc59e699b04816241c3bfe380659d2bc6e52fe
vitaliksmile <vitaliksmile777@gmail.com>
2023-01-27 12:20:53 +0200

    111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
+import Modal from 'Modal/Modal';
+import React, { useState } from 'react';
+import './App.css';
 export const App = () => {
+  const [modalActive, setModalActive] = useState(false);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <main>
+        <button className="open-btn" onClick={() => setModalActive(true)}>
+          Open modal
+        </button>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit ut qui quasi ad molestias, iste nihil repudiandae earum,
+          vitae, incidunt sed harum! Pariatur alias perspiciatis repellendus
+          quidem incidunt itaque vitae.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit ut qui quasi ad molestias, iste nihil repudiandae earum,
+          vitae, incidunt sed harum! Pariatur alias perspiciatis repellendus
+          quidem incidunt itaque vitae.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit ut qui quasi ad molestias, iste nihil repudiandae earum,
+          vitae, incidunt sed harum! Pariatur alias perspiciatis repellendus
+          quidem incidunt itaque vitae.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit ut qui quasi ad molestias, iste nihil repudiandae earum,
+          vitae, incidunt sed harum! Pariatur alias perspiciatis repellendus
+          quidem incidunt itaque vitae.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit ut qui quasi ad molestias, iste nihil repudiandae earum,
+          vitae, incidunt sed harum! Pariatur alias perspiciatis repellendus
+          quidem incidunt itaque vitae.
+        </p>
+      </main>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error itaque,
+          alias delectus beatae enim blanditiis accusamus consequatur quisquam,
+          quae ab eius in voluptatem. Nostrum omnis commodi eligendi itaque
+          voluptates id?
+        </p>
+      </Modal>
     </div>
   );
 };
